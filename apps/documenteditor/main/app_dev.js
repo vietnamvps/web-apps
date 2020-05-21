7d435bc0456fc591bbc5c16936099c026e6764e0
JuliaSvinareva <Julia.Svinaryova@onlyoffice.com>
2020-05-21 19:29:40 +0300

    update bootstrap

--- apps/documenteditor/main/app_dev.js
+++ apps/documenteditor/main/app_dev.js
@@ -48,6 +48,7 @@ require.config({
         jquery          : '../vendor/jquery/jquery',
         underscore      : '../vendor/underscore/underscore',
         backbone        : '../vendor/backbone/backbone',
+        popper          : '../vendor/popper/popper',
         bootstrap       : '../vendor/bootstrap/dist/js/bootstrap',
         text            : '../vendor/requirejs-text/text',
         perfectscrollbar: 'common/main/lib/mods/perfect-scrollbar',
@@ -81,7 +82,8 @@ require.config({
         },
         bootstrap: {
             deps: [
-                'jquery'
+                'jquery',
+                'popper'
             ]
         },
         perfectscrollbar: {
